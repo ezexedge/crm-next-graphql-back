b47bc4694db0a1eed23c20da884ea3bf622d8b64
ezequiel gallardo <ezeedge@gmail.com>
2021-02-27 00:03:10 -0300

    creando usuarios

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,6 +1,41 @@
+const Usuario = require('../models/Usuario')
+const bcryptjs = require('bcryptjs')
+
 const resolvers = {
     Query :  {
         obtenerCurso: () => "algo"
+    },
+
+    Mutation : {
+        nuevoUsuario : async (_, {input}) => {
+            
+            const {email,password} = input
+
+            const existeUsuario = await Usuario.findOne({email}).exec()
+            console.log(existeUsuario)
+            if(existeUsuario){
+                throw new Error('el usuario no existe')
+            }
+
+
+            const salt = await bcryptjs.genSalt(10)
+            input.password = await bcryptjs.hash(password , salt)
+
+            console.log('-----hahs---',input.password)
+
+            try{
+
+                const usuario = new Usuario(input)
+
+                usuario.save()
+
+                return usuario
+
+            }catch(error){
+                console.log(error)
+            }
+
+        } 
     }
 }
 
